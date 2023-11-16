b1efe9167c24e3d291dde82fd72fa518bb50c575
cmkane1982 <113553448+cmkane1982@users.noreply.github.com>
2023-11-15 23:19:25 -0800

    Created Hour20

--- ckane_Hour17and18/Assets/Scripts/AnimationTransitions.cs
+++ ckane_Hour17and18/Assets/Scripts/AnimationTransitions.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AnimationTransitions : MonoBehaviour
 {
     Animator anim;
+    //private char keyCode;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,27 @@ public class AnimationTransitions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if (Input.inputString != "")
+        //{
+        //    keyCode = Input.inputString[0];
+        //}
+
+        //switch(keyCode)
+        //{
+        //    case 'A':
+        //        anim.SetTrigger("Color");
+        //        break;
+        //    case 'D':
+        //        anim.SetTrigger("Scale");
+        //        break;
+        //    case 'S':
+        //        anim.SetTrigger("Spin");
+        //        break;
+        //    case 'W':
+        //        anim.SetTrigger("Hover");
+        //        break;
+        //}
+
         if (Input.GetKeyDown(KeyCode.A))
         {
             anim.SetTrigger("Color");
